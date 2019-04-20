e70f5ed402f6ccc0507000f02012ba92e8427ac1
joy <qiaojoy99@163.com>
2019-04-20 19:34:12 +0800

    配置freemarker

---
+++ src/main/java/com/joy/jweb/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.joy.jweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index")
+    public ModelAndView index(){
+        ModelAndView modelAndView = new ModelAndView("/index");
+        return modelAndView;
+    }
+
+}
